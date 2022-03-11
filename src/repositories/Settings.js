68e5838ce9c96d8b36a5bd7456c74a5e442279b0
David Clark <david.clark.1043@gmail.com>
2022-03-11 12:39:25 -0600

    Fixed extra character in digitalocean url

--- src/repositories/Settings.js
+++ src/repositories/Settings.js
@@ -1,4 +1,4 @@
 export default {
-    remoteURL: "https://on-the-hunt-api-eimgi.ondigitalocean.app/",
+    remoteURL: "https://on-the-hunt-api-eimgi.ondigitalocean.app",
     API_KEY: process.env.MAPS_API_KEY
 }
