9e8ac6fa1a3be87e67091a6a62307c6f09808151
David Clark <david.clark.1043@gmail.com>
2022-03-18 15:57:44 -0500

    Added drag and drop ability on clue list pages

--- src/components/huntCreator/HuntCreator.js
+++ src/components/huntCreator/HuntCreator.js
@@ -56,16 +56,135 @@ export const HuntCreator = (props) => {
                 if (typeof filteredHunts[0] === 'object') {
                     setCurrentUserHunts(filteredHunts)
                 } else {
-                    setCurrentUserHunts([<div>No users hunting this scavenger hunt.</div>])
+                    setCurrentUserHunts([<div key={"hunter--0"}>No users hunting this scavenger hunt.</div>])
                 }
             }
         }, [props]
     )
 
+
+
     useEffect(
         () => {
-            if (editingClue) {
+            /*
+                handleDragStart adds the dragged clue to datatransfer and makes the clue semi transparent
+            */
+            const handleDragStart = (event, clue) => {
+                event.dataTransfer.setData("clue", JSON.stringify(clue))
+
+                //event.dataTransfer.setDragImage(dragImage, 0, 0)
+                setTimeout(() => {
+                    event.target.classList.add('hide');
+                }, 0);
+            }
+
+            // stops handleDragOver from triggering
+            const handleDragOver = (event) => {
+                event.preventDefault()
+            }
+
+            const handleDrop = (event, status) => {
+                if(event.target.className.includes("singleStep")) {
+                    const el = event.target
+                    el.classList.remove("over")
+                }
+                const targetId = parseInt(event.currentTarget.id.split("--")[1])
+                const draggedClue = JSON.parse(event.dataTransfer.getData("clue"))
+                const draggedDiv = document.querySelector(`#clue--${draggedClue.id}`)
+                setTimeout(() => {
+                    draggedDiv.classList.remove("hide")
+                })
+                const copy = props.clues.map(clue => ({...clue}))
+                const targetClue = JSON.parse(JSON.stringify(copy.find(clue => clue.id === targetId)))
+
+                const draggedIndex = draggedClue.clueIndex
+                const targetIndex = targetClue.clueIndex
+                // goal: get draggedClue where targetClue is located
+                //  update in between clues to new index values
+                // if target === dragged clue do nothing
+                if(targetClue.id !== draggedClue.id) {
+                    // make draggedClue's index = targetClue's index
+                    draggedClue.clueIndex = targetIndex
+                    // get clue array w/o draggedClue
+                    const filteredClues = copy.filter(clue => clue.id !== draggedClue.id)
+                    // iterate over new arry
+                    if(draggedIndex > targetIndex) {
+                        for (const clue of filteredClues) {
+                            // if clueIndex is >= targetClue and < draggedClue
+                            if(clue.clueIndex >= targetIndex && 
+                                clue.clueIndex < draggedIndex) {
+                                    // increment clueIndex
+                                    clue.clueIndex++
+                                }
+                                // else do nothing
+                        }
+                    } else {
+                        // if target is greater than dragged clue index
+                        for (const clue of filteredClues) {
+                            if(clue.clueIndex <= targetIndex && 
+                                clue.clueIndex > draggedIndex) {
+                                clue.clueIndex--
+                            }
+                        }
+                    }
+                    // add draggedClue back to array
+                    filteredClues.push(draggedClue)                
+                    // sort on clueIndex
+                    filteredClues.sort((firstClue, secondClue) => firstClue.clueIndex - secondClue.clueIndex)
+                    // send to database
+                    let sendArr = []
+                    for (const clue of filteredClues) {
+                        delete clue.clueType
+                        sendArr.push(ClueRepository.updateClue(clue))
+                    }
+                    Promise.all(sendArr)
+                        .then(() => {
+                            return ClueRepository.getCluesForHunt(props.hunt.id)
+                        })
+                        .then(props.setClues)
+                    // set as cluearray
+                }
+            }
+
+            const handleEnter = (event) => {
+                event.preventDefault()
+                if(event.target.className.includes("singleStep")) {
+                    const overNodes = Array.from(document.querySelectorAll(".over"))
+                    for (const node of overNodes) {
+                        node.classList.remove("over")
+                    }
+                    const el = event.target
+                    el.classList.add("over")
+                }
+            }
+
+            const handleLeave = (event) => {
+                event.preventDefault()
+                if(event.relatedTarget.className.includes("stepList") ||
+                    event.relatedTarget.className.includes("huntBox")) {
+                    const el = event.target
+                    el.classList.remove("over")
+                }
+            }
+
+            const handleEnd = (event) => {
+                event.preventDefault()
+                const overNodes = Array.from(document.querySelectorAll(".over"))
+                for (const node of overNodes) {
+                    node.classList.remove("over")
+                }
+
+                if(event.target.className.includes("hide")) {
+                    const el = event.target
+                    el.classList.remove("hide")
+                }
+                
+            }
+
+            if (editingClue)
+             {
                 const clueJS = [<ClueEditing
+                    key={"clueEdit--0"}
                     currentClue={currentClue}
                     setCurrentClue={setCurrentClue}
                     handleClueInput={handleClueInput}
@@ -77,24 +196,36 @@ export const HuntCreator = (props) => {
             } else {
                 if (props.clues) {
                     const clueJS = props.clues.map((clue, index) => {
-                        return <section className="singleStep" key={`clue--${clue.id}`}>
-                            <div className="stepInfo">
-                                <div className="clueStats">
-                                    <div>Clue {index + 1}</div>
-                                    <div>{clue.clueType?.type} clue</div>
-                                </div>
-                                <div className="clueText">Hint: {clue.clueText}</div>
-                                <div className="clueAnswer">Answer: {clue.clueAnswer}</div>
-                            </div>
-                            <div className="stepButtons">
-                                <button className="stepEdit" id={`${clue.id}`} onClick={openEditor}>
-                                    Edit
-                                </button>
-                                <button className="stepDelete" id={`${clue.id}`} onClick={deleteClue}>
-                                    Delete
-                                </button>
-                            </div>
-                        </section>
+                        return <section
+                                    className="singleStep"
+                                    key={`clue--${clue.id}`}
+                                    id={`clue--${clue.id}`}
+                                    draggable
+                                    onDragStart={(e) => handleDragStart(e, clue)}
+                                    onDragOver={(e) => handleDragOver(e)}
+                                    onDrop={(e) => handleDrop(e, "pending")}
+                                    onDragEnter={(e) => handleEnter(e)}
+                                    //onDragOver={(e) => handleOver(e)}
+                                    onDragLeave={(e) => handleLeave(e)}
+                                    onDragEnd={(e) => handleEnd(e)}
+                                    >
+                                    <div className="stepInfo">
+                                        <div className="clueStats">
+                                            <div>Clue {index + 1}</div>
+                                            <div>{clue.clueType?.type} clue</div>
+                                        </div>
+                                        <div className="clueText">Hint: {clue.clueText}</div>
+                                        <div className="clueAnswer">Answer: {clue.clueAnswer}</div>
+                                    </div>
+                                    <div className="stepButtons">
+                                        <button className="stepEdit" id={`${clue.id}`} onClick={openEditor}>
+                                            Edit
+                                        </button>
+                                        <button className="stepDelete" id={`${clue.id}`} onClick={deleteClue}>
+                                            Delete
+                                        </button>
+                                    </div>
+                                </section>
                     })
                     setClueJSX(clueJS)
                 }
@@ -150,7 +281,7 @@ export const HuntCreator = (props) => {
     }
 
     const deleteClue = (event) => {
-        if(props.clues.length === 1) {
+        if (props.clues.length === 1) {
             window.alert("You can't delete the last clue. Delete the hunt if you would like to remove it.")
         } else {
             const copyClues = props.clues.map(clue => ({ ...clue }))
